97f0d8ee741fba1369f4aed01f29f88de7dba617
Manh Cuong <mrmanhcuong126@gmail.com>
2023-03-14 17:47:45 +0700

    update code

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import React from "react";
+import "./App.css";
 // import YoutubeList from "./Components/Youtube/YoutubeList";
-import ChangeColorBox from "./Components/ChangeColorBox";
+import ColorBox from "./Components/ChangeColorBox/ColorBox.js";
 const App = () => {
   // const name = "ReyC";
+
   return (
-    <div>
+    <div className="app">
       {/* <YoutubeList>{name}</YoutubeList> */}
-      {/* <YoutubeItem
-        image="https://images.unsplash.com/photo-1444723121867-7a241cacace9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8Y2l0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
-        avatar="https://images.unsplash.com/photo-1570295999919-56ceb5ecca61?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60"
-        title=""
-        author=""
-      />
-      <YoutubeItem
-        image="https://images.unsplash.com/photo-1480714378408-67cf0d13bc1b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8Y2l0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
-        avatar="https://images.unsplash.com/photo-1557862921-37829c790f19?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60"
-        title=""
-        author=""
-      />
-      <YoutubeItem
-        image="https://images.unsplash.com/photo-1449824913935-59a10b8d2000?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8Y2l0eXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=900&q=60"
-        avatar="https://images.unsplash.com/photo-1581382575275-97901c2635b7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=900&q=60"
-        title=""
-        author=""
-      />
-       */}
-      <h1>Welcom React Hook State</h1>
-      <ChangeColorBox />
+      <ColorBox />
+      <h1>My Todos</h1>
     </div>
   );
 };
--- src/Components/ChangeColorBox/ColorBox.js
+++ src/Components/ChangeColorBox/ColorBox.js
@@ -1,30 +1,33 @@
 import React, { useState } from "react";
 import "./ChangeColor.css";
 
-const ChangeColorBox = () => {
+const ColorBox = () => {
   const [color, setColor] = useState(() => {
-    const initColor = localStorage.getItem("box_color") || "orange";
+    const initColor = localStorage.getItem("box-color") || "red";
+    console.log(initColor);
     return initColor;
   });
 
   const randomColor = () => {
-    const COLOR_LIST = ["red", "yellow", "orange", "black", "green"];
+    const COLOR_LIST = ["red", "orange", "black", "yellow", "green"];
     const randomIndex = Math.trunc(Math.random() * 5);
     return COLOR_LIST[randomIndex];
   };
 
-  const handlerChangeColor = () => {
+  const handlClickBox = () => {
     const newColor = randomColor();
     setColor(newColor);
+
+    localStorage.setItem("box-color", newColor);
   };
 
   return (
     <div
       className="color-box"
       style={{ backgroundColor: color }}
-      onClick={handlerChangeColor}
+      onClick={handlClickBox}
     ></div>
   );
 };
 
-export default ChangeColorBox;
+export default ColorBox;
